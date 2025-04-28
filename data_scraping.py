ae9f4a185edbd0f9eceda67302bdf2ece87bf91a
Christian Durante <154074830+ChristianChR@users.noreply.github.com>
2025-04-28 22:45:12 +0200

    Update data_scraping.py

--- data_scraping.py
+++ data_scraping.py
@@ -2,13 +2,8 @@ import requests
 import pandas as pd
 import time
 import os
-import threading
-import keyboard
 from openpyxl import load_workbook, Workbook
 from openpyxl.utils import get_column_letter
-
-stop_fetch = False
-
 def get_user_instagram_id(access_token, page_id):
     url = f"https://graph.facebook.com/v22.0/{page_id}?fields=instagram_business_account&access_token={access_token}"
     response = requests.get(url)
@@ -42,23 +37,22 @@ def get_hashtag_id(access_token, hashtag_name, user_instagram_id):
         print(f"Response content: {response.content}")
         return None
 
-def fetch_top_media_hashtag(access_token, hashtag_id, user_id, start_url=None):
-    global stop_fetch
+def fetch_recent_media_hashtag(access_token, hashtag_id, user_id, start_url=None):
     video_data = []
     url = start_url or f"https://graph.facebook.com/v22.0/{hashtag_id}/recent_media?user_id={user_id}&fields=comments_count,like_count,permalink&access_token={access_token}"
     api_call_count = 0
+    calls_without_results = 0  
 
-    while url and not stop_fetch:
-        #os.system('cls' if os.name == 'nt' else 'clear')
+    while url:
         api_call_count += 1
         print(f"Executing API call... (Call number: {api_call_count})")
         response = requests.get(url)
-        
+
         if response.status_code == 429:
             print("Rate limit exceeded, waiting 3600 seconds...")
             time.sleep(3600)
             continue
-        
+
         if response.status_code != 200:
             print(f"Request error: {response.status_code}")
             try:
@@ -69,15 +63,22 @@ def fetch_top_media_hashtag(access_token, hashtag_id, user_id, start_url=None):
 
         data = response.json()
 
-        for media in data.get("data", []):
-            if "/reel/" in media["permalink"]:
-                video_data.append({
-                    "permalink": media["permalink"],
-                    "comments_count": media.get("comments_count", 0),
-                    "like_count": media.get("like_count", 0)
-                })
-                print(f"REELS INSERTED: {len(video_data)}")
-
+        if data.get("data", []):
+            calls_without_results = 0 
+            for media in data.get("data", []):
+                if "/reel/" in media["permalink"]:
+                    video_data.append({
+                        "permalink": media["permalink"],
+                        "comments_count": media.get("comments_count", 0),
+                        "like_count": media.get("like_count", 0)
+                    })
+                    print(f"REELS INSERTED: {len(video_data)}")
+        else:
+            calls_without_results += 1
+            print(f"No new data found in this call. Calls without results: {calls_without_results}")
+            if calls_without_results >= 5:
+                print("Stopping fetch after 5 calls without results.")
+                break  
         url = data.get('paging', {}).get('next', None)
         if url:
             print(f"Next page URL: {url}")
@@ -89,12 +90,18 @@ def fetch_top_media_hashtag(access_token, hashtag_id, user_id, start_url=None):
         if rate_limit_remaining and int(rate_limit_remaining) < 200:
             print("Close to call limit, pausing for 3600 seconds...")
             time.sleep(3600)
+
     return video_data
+
 
 def save_video_urls_to_excel(video_data, file_name):
     if not video_data:
         print("No video data to save.")
         return
+
+    folder_path = os.path.dirname(file_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
     if os.path.exists(file_name):
         wb = load_workbook(file_name)
@@ -129,26 +136,16 @@ def save_video_urls_to_excel(video_data, file_name):
 
     wb.save(file_name)
     print(f"Saved {len(new_video_data)} new video URLs in {file_name}")
-    stop_fetch = True
 
 def read_access_token(file_path):
     with open(file_path, 'r') as file:
         return file.read().strip()
 
-def check_for_stop():
-    global stop_fetch
-    keyboard.wait('x')
-    stop_fetch = True
-
 if __name__ == "__main__":
-    access_token = read_access_token("c:\\Users\\duran\\Desktop\\credentials.txt")
-    hashtag_name = "mouthtape"
+    access_token = read_access_token("/content/drive/MyDrive/Colab Notebooks/Credentials.txt")
+    hashtag_name = "wisdomteeth"
     user_instagram_id = get_user_instagram_id(access_token, get_page_id(access_token))
     hashtag_id = get_hashtag_id(access_token, hashtag_name, user_instagram_id)
     if hashtag_id:
-        stop_thread = threading.Thread(target=check_for_stop)
-        stop_thread.start()
-        #start_url = input("Enter the next page URL to resume from (or press Enter to start from the beginning): ").strip() or None
-        video_data = fetch_top_media_hashtag(access_token, hashtag_id, user_instagram_id, None)
-        save_video_urls_to_excel(video_data, f"c:\\Users\\duran\\Desktop\\{hashtag_name}_urls.xlsx")
-        stop_thread.join()
+        video_data = fetch_recent_media_hashtag(access_token, hashtag_id, user_instagram_id)
+    save_video_urls_to_excel(video_data, f"/content/drive/MyDrive/Colab Notebooks/{hashtag_name}_urls.xlsx")
